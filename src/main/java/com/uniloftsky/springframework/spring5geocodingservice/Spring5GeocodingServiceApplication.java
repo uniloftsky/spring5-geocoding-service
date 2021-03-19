2be00a3c72cdb98d0279b6683680a70f4f1f1b18
uniloftsky <uniloftsky@gmail.com>
2021-03-20 00:11:31 +0200

    project added

---
+++ src/main/java/com/uniloftsky/springframework/spring5geocodingservice/Spring5GeocodingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.uniloftsky.springframework.spring5geocodingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5GeocodingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring5GeocodingServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/uniloftsky/springframework/spring5geocodingservice/Spring5GeocodingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniloftsky.springframework.spring5geocodingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5GeocodingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
